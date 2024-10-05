c3ced76387a4b2dd485f54e690bc1cd4c75e4870
Likith <Likithu@necun.in>
2024-10-05 21:44:25 +0530

    Initial Commit

---
+++ ARKIT _DEMO/ARKIT__DEMOApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARKIT__DEMOApp.swift
+//  ARKIT _DEMO
+//
+//  Created by Likith Undabhatla on 05/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ARKIT__DEMOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
