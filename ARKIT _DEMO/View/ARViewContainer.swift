879b04a06fb63b8ea482b6ad4619f03e11d8108e
LikhithDev2003 <likhithiosdev@gmail.com>
2024-12-29 22:53:06 +0530

    Update ARViewContainer.swift

--- ARKIT _DEMO/View/ARViewContainer.swift
+++ ARKIT _DEMO/View/ARViewContainer.swift
@@ -1,22 +1,2 @@
-//
-//  ARViewContainer.swift
-//  ARKIT _DEMO
-//
-//  Created by Likith Undabhatla on 05/10/24.
-//
 
-//import SwiftUI
-//import RealityKit
-//
-//struct ARViewContainer: UIViewRepresentable {
-//    @ObservedObject var viewModel: ARViewModel
-//
-//    func makeUIView(context: Context) -> ARView {
-//        viewModel.arView ?? ARView(frame: .zero)
-//    }
-//
-//    func updateUIView(_ uiView: ARView, context: Context) {
-//        // No update logic required here
-//    }
-//}
 
